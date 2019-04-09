47758e6a0239429b17c1f1b78993fc60249674df
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-04-09 10:08:18 -0300

    create initial backend

---
+++ backend/src/routes.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const routes = express.Router();
+
+routes.get('/teste', (req, res)=>{
+
+    return res.send('Hello World');
+})
+
+
+module.exports = routes;
